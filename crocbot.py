2a6316606243d6b916b9fe44adcdb8daf4732495
is386 <20195347+is386@users.noreply.github.com>
2020-02-06 23:41:58 -0500

    add remove role cmd

--- crocbot.py
+++ crocbot.py
@@ -3,12 +3,14 @@ from discord.ext import commands
 from yaml import safe_load
 
 from secret import token
-from crocomire import embeds, memes, boost
+from crocomire import embeds, memes, boost, roles
 
 prefix: str = "?"
 croc_emote: str = "<:Crocomire:583880666970718224>"
 status_msg: str = "Bruh, Type ?info"
 top10_msg: str = "**Top 10 Ridleycord Boosters**```{}```"
+role_msg: str = "I removed the role **{}** from these users Bruh {}:\n```{}```"
+role_error: str = "{} you need the permission **Administrator** to remove the role Bruh {}"
 cmd_data: dict = safe_load(open("commands.yml"))
 
 bot: commands.Bot = commands.Bot(
@@ -27,8 +29,7 @@ async def send_help(ctx: commands.Context):
     """
     embed = embeds.create_text_embed(cmd_data["info"])
     await ctx.author.send(embed=embed)
-    await ctx.send(
-        "{} Bruh, I sent you a DM {}".format(ctx.author.mention, croc_emote))
+    await ctx.send("{} Bruh, I sent you a DM {}".format(ctx.author.mention, croc_emote))
 
 
 @bot.command(aliases=cmd_data["commands"])
@@ -86,5 +87,39 @@ async def send_leaderboard(ctx: commands.Context):
         c += 1
 
     await ctx.send(top10_msg.format(s))
+
+
+@bot.command(name="removerole")
+@commands.has_permissions(administrator=True)
+async def remove_role(ctx: commands.Context):
+    """
+    Async function that removes the given role from all users.
+
+    :param ctx: `commands.Context`
+    :return: `None`
+    """
+    if "jmu" not in "".join(ctx.message.content.split()[1:]).lower():
+        await ctx.send("That is not a JMU role Bruh {}".format(croc_emote))
+        return
+
+    role_name, members = await roles.remove_all_roles(ctx)
+    if len(members) == 0:
+        await ctx.send("No one had the role **{}** Bruh {}".format(role_name, croc_emote))
+        return
+
+    await ctx.send(role_msg.format(role_name, croc_emote, members))
+
+
+@remove_role.error
+async def remove_role_error(ctx: commands.Context, error: commands.CommandError):
+    """
+    Async function to send a message if a user is missing permissions.
+
+    :param ctx: `Context` original user message's context
+    :param error: `commands.CommandError` the error invoked by the user
+    :return: `None`
+    """
+    if isinstance(error, commands.MissingPermissions):
+        await ctx.send(role_error.format(ctx.author.mention, croc_emote))
 
 bot.run(token)
---
+++ crocomire/roles.py
@@ -0,0 +1,32 @@
+from typing import List
+
+from discord import Role
+from discord.ext import commands
+
+
+async def remove_all_roles(ctx: commands.Context):
+    """
+    Async function that removes the given role from all users.
+
+    :param ctx: `commands.Context`
+    :return: `str`, `str`
+    """
+    role: Role = None
+    users: str = ""
+    role_name: str = "".join(ctx.message.content.split()[1:]).lower()
+    roles: List[Role] = await ctx.guild.fetch_roles()
+
+    for r in roles:
+        r_name: str = "".join(str(r).split()).lower()
+        if r_name == role_name:
+            role = r
+            break
+
+    if role is None:
+        return role_name, users
+
+    for m in role.members:
+        await m.remove_roles(role)
+        users += str(m) + ", "
+
+    return str(role), users
