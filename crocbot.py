1fd8bd4515e6f0930ddc6396969acc5ea8f1183e
is386 <20195347+is386@users.noreply.github.com>
2020-03-23 12:00:05 -0400

    fix length msg error check

--- crocbot.py
+++ crocbot.py
@@ -258,7 +258,7 @@ async def cmd_error(ctx: commands.Context, error: commands.CommandError):
 
 @bot.event
 async def on_message(msg: Message):
-    if len(msg.content) != 0 or bot.user == msg.author or msg.content[0] != prefix:
+    if len(msg.content) == 0 or bot.user == msg.author or msg.content[0] != prefix:
         return
 
     cmd: str = msg.content.split()[0][1:]
