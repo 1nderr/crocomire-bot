044cc5b7b22aa1ef5532a746b533bed848bedb0e
is386 <20195347+is386@users.noreply.github.com>
2020-11-22 20:15:03 -0500

    fix mash command

--- crocomire/cogs/fun.py
+++ crocomire/cogs/fun.py
@@ -68,7 +68,7 @@ class Fun(commands.Cog):
         await ctx.send(embed=embed)
 
     @commands.command(name="mash")
-    async def send_mash(ctx: commands.Context):
+    async def send_mash(self, ctx: commands.Context):
         await ctx.send("".join([choice(list(ascii_letters)) for i in range(randint(30, 76))]))
 
     @add_meme.error
