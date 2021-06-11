cb9aacd11e9036aa6597acb224af4a4e7e62080d
is386 <20195347+is386@users.noreply.github.com>
2021-06-10 21:03:20 -0400

    add member join message

--- crocbot.py
+++ crocbot.py
@@ -85,6 +85,11 @@ async def on_command_error(ctx, error):
     raise error
 
 
+@bot.event
+async def on_member_join(member):
+    await member.send("new fren!")
+
+
 async def self_msg(msg: str):
     ridcord: Guild = bot.get_guild(456142548667465728)
     chan_id: int = 456260916720173057
